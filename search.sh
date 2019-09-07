87f06750f65381fa39c217249bbc51913bda3d84
Anushaaravind <anuarav1214@gmail.com>
2019-09-07 21:47:17 +0530

    modified

--- search.sh
+++ search.sh
@@ -1,4 +1,4 @@
 #program to search a particular string in a text file and print the name of the text file
 
 #grep "devops" ~/*.txt
-grep   "software" /home/vagrant/shellscripts/*.txt
+grep   "software" /var/lib/jenkins/workspace/test_pipeline_practice/*.txt
