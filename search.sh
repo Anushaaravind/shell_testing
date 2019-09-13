d2b1d8aea89e5841641b05af58d6e5fafd70a362
Anushaaravind <anuarav1214@gmail.com>
2019-09-13 19:48:50 +0530

    modified

--- search.sh
+++ search.sh
@@ -1,4 +1,5 @@
 #program to search a particular string in a text file and print the name of the text file
 
 #grep "devops" ~/*.txt
-grep   "software" /var/lib/jenkins/workspace/test_pipeline_practice/*.txt
+val=$(grep  -n  "development" /var/lib/jenkins/workspace/test_pipeline_practice/*.txt)
+echo $val | cat results.txt
